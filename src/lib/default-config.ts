bb9947c9ec2eb12b4ba8b2ca932252e18d1d43d4
Daniel Causebrook <daniel@causebrook.co.uk>
2025-05-27 14:15:06 +0100

    Improve default config

--- src/lib/default-config.ts
+++ src/lib/default-config.ts
@@ -10,28 +10,32 @@ export default {type: "chain", data: [
         're', 'un', 'de', 'retro', 'super', 'sub', 'con', 'circum', 'auto', 'contra',
         'trans', 'dis', 'exo', /*'extra', */'inter',
     ]},
-    '|',
+    '|', // Prefix separator
     {type: 'random', data: [
         '', '', '', '', '', '', '',
         'en',
     ]},
-    '|',
+    '|', // Prefix separator
     {type: 'random', data: [
         // '~' After a vowel signifies it has to be a long vowel.
         'baffl', 'bant', 'bash', 'beam', 'bee~p', 'bi~nd', 'blast', 'blip', 'bonk', 'bug', 'buzz', 'burn', 'bust', 'cab', 'crank', 'crack', 'crash',
-        'disc', 'dri~v', 'duc', 'duck', 'fab', 'gram', 'mo~t', 'nai~l', 'lash', 'loca~t', 'pact', 'pi~l', 'puck', 'quack',
-        'rot', 'rust', 'scan', 'shred', 'sink', 'skel', 'skim', 'tank', 'tack', 'tact', 'tract', 'turb',
-        'volt', 'warp', 'whack', 'wham', 'whizz', 'wrangl', 'zap', 'zing',
+        'disc', 'drill', 'dri~v', 'duc', 'duck', 'fab', 'frazzl', 'gram', 'mod', 'mo~t', 'nai~l', 'lash', 'loca~t', 'pact', 'pi~l', 'puck', 'quack',
+        'rot', 'rust', 'scan', 'shred', 'sink', 'skelet', 'skim', 'tank', 'tack', 'tact', 'tract', 'trash', 'turb',
+        'volt', 'warp', 'whack', 'wham', 'whizz', 'woo~f', 'wrangl', 'zap', 'zing',
     ]},
-    '&',
+    {type: 'random', data: [
+        '', '', '', '', '', '', '', '', '', '', '',
+        '&u~la~t',
+    ]},
+    '&', // Suffix boundary marker
     {type: 'random', data: [
         'er',
-        'er', 'tor', 'ler', 'ifier', 'tron',
-        'er', 'tor', 'ler', 'ifier', 'tron',
-        'or', 'on', 'ter', 'ton', 'lon', 'lor', 'meter', 'matic',
-        // 'iser',
+        'er', 'tor', 'ifier', 'tron',
+        'er', 'tor', 'ifier', 'tron',
+        'on', 'ter', 'ton', 'meter', 'matic',
+        // Rejects: 'iser', 'ler', 'lon', 'lor', 'or'
     ]},
-    '#',
+    '#', // Word end marker
     {type: 'random', data: [
         '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
         '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
@@ -43,7 +47,7 @@ export default {type: "chain", data: [
         ['([^lm])&\\1+', '$1&'], // De-double letters on suffix boundary
 
         // Word end
-        ['([^aeiouy][blmpt]|[dvz])&(t[oe]r#)', {
+        ['([^aeiouy][blmpt]|[dvz])&(t[oe][rn]#)', {
             type: 'random', data: [
                 '$1&a$2', '$1&o$2', '$1&i$2', '$1&u$2'
             ]
@@ -55,13 +59,19 @@ export default {type: "chain", data: [
         }],
         ['([^l][^aeioy])&meter#', '$1&ometer#'],
         ['scer#', 'cer#'],
-        ['(d|fl|gl|ct|rk|m|v|z)&(tron#|matic#)', {
+        ['&(tron#|matic#)', { // Randomly prefix 'tron' or 'matic' with an 'o'
             type: 'random', data: [
-                '$1&a$2', '$1&o$2'
+                '&$1', '&$1', '&$1', '&$1', '&$1', '&$1', '&$1',
+                '&o$1',
             ]
         }],
-        ['([^aeiou][aei])([mnpbdlz])&(l?[aeiou])', '$1$2$2&$3'], // Double some consonants
-        ['([^aeiou][aeiou])([g])&(l?[eo])', '$1$2$2&$3'], // Double some consonants
+        ['(d|[fgz]l|[aceiou]t|rk|m|v|z)&(tron#|matic#)', {
+            type: 'random', data: [
+                '$1&a$2', '$1&o$2', '$1&o$2',
+            ]
+        }],
+        ['([^aeiou][aei])([mnpbdlz])&(l?[aeiou][^~])', '$1$2$2&$3'], // Double some consonants
+        ['([^aeiou][aeiou])([g])&(l?[eo][^~])', '$1$2$2&$3'], // Double some consonants
         // ['([^aeiou][aei])([mnpbdlz])(l?[aeiou](?:r|t|fier|tron|matic|meter)#)', '$1$2$2$3'], // Double some consonants
         // ['([^aeiou][aeiou])([g])(l?[eo][rt]#)', '$1$2$2$3'], // Double some consonants
         ['&', ''], // Remove suffix boundary marker
